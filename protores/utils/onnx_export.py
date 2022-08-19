ffc3f30e863317de8a525755c4f1c01ab1f0eedf
Boris Oreshkin <boris.oreshkin@unity3d.com>
2022-08-18 20:18:08 -0400

    update

--- protores/utils/onnx_export.py
+++ protores/utils/onnx_export.py
@@ -1,7 +1,24 @@
-from typing import Dict
+from typing import Dict, Optional
 
 import torch
 import onnx
+
+from pytorch_lightning.callbacks import Callback
+
+
+class ModelExport(Callback):
+    def __init__(self, dirpath: str = "", filename: Optional[str]=None, period: int=1):
+        super().__init__()
+        self.period = period
+        self.dirpath = dirpath
+        self.filename = filename
+        if self.filename is None:
+            self.filename = "model_{0}.onnx"
+
+    def on_train_epoch_end(self, trainer, pl_module):
+        if trainer.current_epoch % self.period == 0:
+            filepath = self.dirpath + "/" + self.filename.format(trainer.current_epoch)
+            pl_module.export(filepath)
 
 
 class OnnxWrapper(torch.nn.Module):

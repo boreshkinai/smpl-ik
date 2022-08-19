df3653eefc6541a6fb852e193306338a6032866a
Boris Oreshkin <boris.oreshkin@unity3d.com>
2022-08-18 20:13:46 -0400

    update

--- protores/utils/onnx_export.py
+++ protores/utils/onnx_export.py
@@ -1,7 +1,7 @@
-from typing import Optional
+from typing import Dict
 
 import torch
-from pytorch_lightning import Callback
+import onnx
 
 
 class OnnxWrapper(torch.nn.Module):
@@ -10,7 +10,8 @@ class OnnxWrapper(torch.nn.Module):
         self.module = module
 
         for k in list(dummy_input.keys()):
-            dummy_input[k] = dummy_input[k].to(module.device)
+            if isinstance(dummy_input[k], torch.Tensor):
+                dummy_input[k] = dummy_input[k].to(module.device)
 
         self.dummy_output = module(dummy_input)
         self.input_names = list(dummy_input.keys())
@@ -23,7 +24,7 @@ class OnnxWrapper(torch.nn.Module):
     def forward(self, inputs):
         named_input = {}
         for i in range(len(inputs)):
-            named_input[self.input_names[i]] = inputs[i].to(self.module.device)
+            named_input[self.input_names[i]] = inputs[i].to(self.module.device) if isinstance(inputs[i], torch.Tensor) else inputs[i]
 
         named_output = self.module(named_input)
         outputs = [named_output[k] for k in self.output_names]
@@ -34,21 +35,32 @@ class OnnxWrapper(torch.nn.Module):
                           output_names=self.output_names, *args, **kwargs)
 
 
-def export_named_model_to_onnx(module, dummy_input, filepath, *args, **kwargs):
+def export_named_model_to_onnx(module: torch.nn.Module, dummy_input: torch.Tensor, filepath: str, metadata: Dict[str, str] = None, *args, **kwargs):
+
     wrapped_module = OnnxWrapper(module, dummy_input)
     wrapped_module.export(filepath, *args, **kwargs)
 
+    if metadata is not None:
+        add_metadata(onnx_filepath=filepath, metadata=metadata)
 
-class ModelExport(Callback):
-    def __init__(self, dirpath: str = "", filename: Optional[str]=None, period: int=1):
-        super().__init__()
-        self.period = period
-        self.dirpath = dirpath
-        self.filename = filename
-        if self.filename is None:
-            self.filename = "model_{0}.onnx"
 
-    def on_train_epoch_end(self, trainer, pl_module, outputs):
-        if trainer.current_epoch % self.period == 0:
-            filepath = self.dirpath + "/" + self.filename.format(trainer.current_epoch)
-            pl_module.export(filepath)
+# Adds meta data to an existing model
+# This will load and re-export the original model
+# onnx_filepath: the input ONNX file
+# output_filepath: the output ONNX file. If None, input file will be replaced
+# args, kwards: any other argument used to save the modified model. See onnx.save_model for a list of possibilities
+def add_metadata(onnx_filepath: str, metadata: Dict[str, str], output_filepath:str = None, *args, **kwargs):
+    if output_filepath is None:
+        output_filepath = onnx_filepath
+
+    # load model
+    model = onnx.load(onnx_filepath)
+
+    # add meta-data
+    for key in metadata:
+        meta = model.metadata_props.add()
+        meta.key = key
+        meta.value = metadata[key]
+
+    # save modified model
+    onnx.save(model, output_filepath, *args, **kwargs)
